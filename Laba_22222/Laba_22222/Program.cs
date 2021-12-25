68e60bec271adab7ca94d2f150c02f6a9876ffe9
Чижевський Дмитрій <95354969+ChyzhevskyiD@users.noreply.github.com>
2021-12-25 15:50:22 +0200

    laba_2
    laba_2

---
+++ Laba_22222/Laba_22222/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[4];
+            Random random = new Random();
+            Console.WriteLine("Source array: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(10);
+                Console.Write(array[i] + " ");
+            }
+            array.geometric_and_arithmetic_difference();
+        }
+    }
+    public static class My_class
+    {
+        public static void geometric_and_arithmetic_difference(this int[] arr)
+        {
+            double arithmetic = 0;
+            double geometric = 1;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arithmetic += arr[i];
+                geometric *= arr[i];
+            }
+            arithmetic = arithmetic / arr.Length;
+            Console.WriteLine("\narithmetic_difference:");
+            Console.WriteLine(arithmetic);
+            geometric = Math.Pow(geometric, 1.0/ arr.Length);
+            Console.WriteLine("\ngeometric_difference:");
+            Console.WriteLine(geometric);
+        }
+    }
+}
